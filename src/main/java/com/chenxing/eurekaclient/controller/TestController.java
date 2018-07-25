f6b8b50498e2ea9468c612eea4ad1bf53887be7f
liuxingpc <liuxingpc@DESKTOP-9FM73MU>
2018-07-25 19:09:14 +0800

    aaaa

--- src/main/java/com/chenxing/eurekaclient/controller/TestController.java
+++ src/main/java/com/chenxing/eurekaclient/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.chenxing.eurekaclient.controller;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +25,18 @@ public class TestController {
 	@RequestMapping("/hi")
 	public String sayHi(@RequestParam String name) {
 		String newName = tService.test1(name);
-		return "hi " + newName + "。我是" + appname + " 我的端口号:" + port;
+
+		return "hi " + newName + "。我是" + appname + " 我的端口号:" + port + getip();
+	}
+
+	private String getip() {
+		try {
+			InetAddress address = InetAddress.getLocalHost();
+			return address.getHostAddress();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
